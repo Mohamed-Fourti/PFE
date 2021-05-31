eed6c174d7dbb18f4f9ecb3d989a31f27c15330b
safa-lahwel <59705299+safa-lahwel@users.noreply.github.com>
2021-05-31 16:01:32 -0700

    card

--- resources/views/AdminPanel/layout.blade.php
+++ resources/views/AdminPanel/layout.blade.php
@@ -16,7 +16,7 @@
   <link rel="stylesheet" type="text/css" href="{{ asset('DataTables/datatables.min.css') }}"/>
 
 
-  <script src="{{ asset('js/app.js') }}" defer></script>
+  <script src="{{ asset('js/app.js') }}" ></script>
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
   <script src="{{ asset('js/jquery-3.6.0.min.js') }}" defer></script>
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
+            <div class="card" id="cardAuth">
                 <div class="card-header">{{ __('Renouvellement du mot de passe') }}</div>
 
                 <div class="card-body">
